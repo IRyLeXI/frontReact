b6cca472d6c1eb4f958c2923cb6fc473c01b5ebc
Денис Станіслав <125273149+denstan2004@users.noreply.github.com>
2023-10-10 00:20:49 +0300

    changed massage4

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios'; // Імпорт Axios
 
 function App() {
-    const [message, setMessage] = useState(''); // Стан для зберігання рядка
+    const [numbers, setNumbers] = useState([]); // Стан для зберігання чисел
 
     useEffect(() => {
         // Виконуємо GET-запит до бекенду після завантаження компонента
         axios.get('https://testapp2004.azurewebsites.net/Home/getAll/Clothes')
             .then(response => {
                 // Отримали дані від бекенду
-                setMessage(response.data); // Зберігаємо рядок в стані message
+                setNumbers(response.data);
             })
             .catch(error => {
                 console.error('Помилка отримання даних від сервера:', error);
@@ -19,8 +19,12 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <p>Рядок з сервера:</p>
-                <p>{message}</p> {/* Відображаємо рядок */}
+                <p>Список чисел з сервера:</p>
+                <ul>
+                    {numbers.map((number, index) => (
+                        <li key={index}>{number}</li>
+                    ))}
+                </ul>
             </header>
         </div>
     );
