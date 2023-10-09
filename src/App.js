e8480234830af76b181786f71c111c725a3c3395
Денис Станіслав <125273149+denstan2004@users.noreply.github.com>
2023-10-09 16:18:27 +0300

    changed massage2

--- src/App.js
+++ src/App.js
@@ -2,28 +2,32 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios'; // Імпорт Axios
 
 function App() {
-    const [message, setMessage] = useState(''); // Стан для зберігання рядка
+  const [numbers, setNumbers] = useState([]); // Стан для зберігання чисел
 
-    useEffect(() => {
-        // Виконуємо GET-запит до бекенду після завантаження компонента
-        axios.get('https://testapp2004.azurewebsites.net/Home/getAll/Clothes')
-            .then(response => {
-                // Отримали дані від бекенду
-                setMessage(response.data); // Зберігаємо рядок в стані message
-            })
-            .catch(error => {
-                console.error('Помилка отримання даних від сервера:', error);
-            });
-    }, []);
+  useEffect(() => {
+    // Виконуємо GET-запит до бекенду після завантаження компонента
+    axios.get('https://testapp2004.azurewebsites.net/Home/getAll/Clothes')
+        .then(response => {
+          // Отримали дані від бекенду
+          setNumbers(response.data);
+        })
+        .catch(error => {
+          console.error('Помилка отримання даних від сервера:', error);
+        });
+  }, []);
 
-    return (
-        <div className="App">
-            <header className="App-header">
-                <p>Рядок з сервера:</p>
-                <p>{message}</p> {/* Відображаємо рядок */}
-            </header>
-        </div>
-    );
+  return (
+      <div className="App">
+        <header className="App-header">
+          <p>Список чисел з сервера:</p>
+          <ul>
+            {numbers.map((number, index) => (
+                <li key={index}>{number}</li>
+            ))}
+          </ul>
+        </header>
+      </div>
+  );
 }
 
 export default App;
